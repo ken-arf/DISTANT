21537ce904771aa6d2db4be10744264354483c1a
ken-arf <kenyano@KennoMacBook-Air.local>
2023-02-07 23:08:25 +0900

    commit

--- scripts/jnlpba_preprocess_ds_annotation.sh
+++ scripts/jnlpba_preprocess_ds_annotation.sh
@@ -21,7 +21,8 @@ if [ ! -d $LOG_DIR ]; then
 fi
 
 #nohup python modules/preprocess/annotation/ds_annotation.py --yaml $YAML_PATH > $LOG_PATH &
-python modules/preprocess/annotation/ds_annotation.py --yaml $YAML_PATH 
+#python modules/preprocess/annotation/ds_annotation.py --yaml $YAML_PATH 
+spark-submit modules/preprocess/annotation/ds_annotation_spark.py --yaml $YAML_PATH 
 
 sleep 5
 tail -f $LOG_PATH
