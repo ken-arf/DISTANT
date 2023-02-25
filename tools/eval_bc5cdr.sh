2db39349349774b3a363ff11df2e5aeb4160439b
aae14935wb <aae14935wb@es2.abci.local>
2023-02-26 07:06:26 +0900

    commit

--- tools/eval_bc5cdr.sh
+++ tools/eval_bc5cdr.sh
@@ -5,7 +5,7 @@ ROOT_DIR=../data/BC5CDR/eval
 
 predict_coll_dir=$ROOT_DIR/annotate.prev
 predict_coll_dir=$ROOT_DIR/annotate.latest
-predict_coll_dir=$ROOT_DIR/annotate
+predict_coll_dir=$ROOT_DIR/annotate.0225
 true_coll_dir=$ROOT_DIR/annotate.gold
 
 ls $predict_coll_dir/*.coll | sort | xargs -n1 cat > predict.coll
