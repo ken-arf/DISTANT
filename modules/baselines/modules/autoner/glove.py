a5cd45136be85f4af910f9d1eb7eecb0bcd5fa7e
yano <yano.ken@aist.go.jp>
2023-03-15 13:04:27 +0900

    commit

---
+++ modules/baselines/modules/autoner/glove.py
@@ -0,0 +1,63 @@
+
+words = []
+idx = 0
+word2idx = {}
+vectors = bcolz.carray(np.zeros(1), rootdir=f'{glove_path}/6B.50.dat', mode='w')
+
+
+with open(f'{glove_path}/glove.6B.50d.txt', 'rb') as f:
+    for l in f:
+        line = l.decode().split()
+        word = line[0]
+        words.append(word)
+        word2idx[word] = idx
+        idx += 1
+        vect = np.array(line[1:]).astype(np.float)
+        vectors.append(vect)
+    
+vectors = bcolz.carray(vectors[1:].reshape((400000, 50)), rootdir=f'{glove_path}/6B.50.dat', mode='w')
+vectors.flush()
+pickle.dump(words, open(f'{glove_path}/6B.50_words.pkl', 'wb'))
+pickle.dump(word2idx, open(f'{glove_path}/6B.50_idx.pkl', 'wb'))
+
+vectors = bcolz.open(f'{glove_path}/6B.50.dat')[:]
+words = pickle.load(open(f'{glove_path}/6B.50_words.pkl', 'rb'))
+word2idx = pickle.load(open(f'{glove_path}/6B.50_idx.pkl', 'rb'))
+
+glove = {w: vectors[word2idx[w]] for w in words}
+
+
+matrix_len = len(target_vocab)
+weights_matrix = np.zeros((matrix_len, 50))
+words_found = 0
+
+for i, word in enumerate(target_vocab):
+    try: 
+        weights_matrix[i] = glove[word]
+        words_found += 1
+    except KeyError:
+        weights_matrix[i] = np.random.normal(scale=0.6, size=(emb_dim, ))
+
+
+def create_emb_layer(weights_matrix, non_trainable=False):
+    num_embeddings, embedding_dim = weights_matrix.size()
+    emb_layer = nn.Embedding(num_embeddings, embedding_dim)
+    emb_layer.load_state_dict({'weight': weights_matrix})
+    if non_trainable:
+        emb_layer.weight.requires_grad = False
+
+    return emb_layer, num_embeddings, embedding_dim
+
+class ToyNN(nn.Module):
+    def __init__(self, weights_matrix, hidden_size, num_layers):
+        super(self).__init__()
+        self.embedding, num_embeddings, embedding_dim = create_emb_layer(weights_matrix, True)
+        self.hidden_size = hidden_size
+        self.num_layers = num_layers
+        self.gru = nn.GRU(embedding_dim, hidden_size, num_layers, batch_first=True)
+        
+    def forward(self, inp, hidden):
+        return self.gru(self.embedding(inp), hidden)
+    
+    def init_hidden(self, batch_size):
+        return Variable(torch.zeros(self.num_layers, batch_size, self.hidden_size))
