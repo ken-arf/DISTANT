a4b8fa15eef82fb1277081718a40ac477947ddec
yano <yano.ken@aist.go.jp>
2023-08-17 13:20:35 +0900

    commit

--- modules/finetune/segmentation/make_dataset2.py
+++ modules/finetune/segmentation/make_dataset2.py
@@ -8,6 +8,7 @@ import glob
 import spacy
 import scispacy
 import time
+import json
 import pandas as pd
 from collections import defaultdict
 from tqdm import tqdm
@@ -32,7 +33,11 @@ def sentence_split(doc, offset = False, moses = False):
     else:
 
         doc = nlp(doc)
-        sents =  [sent.text for sent in doc.sents]
+        if offset == False:
+            sents =  [sent.text for sent in doc.sents]
+        else:
+            sents =  [(sent.text, sent.start_char)  for sent in doc.sents]
+
         
     return sents
     
@@ -55,62 +60,39 @@ def tokenize(text, offset = False, moses = False, ):
     return tokens 
     
 
-def load_dict_old(path):
+def annotate_helper_(tokens, entities):
 
-    items = []
-    with open(path) as fp:
-        lines = [line.strip().lower() for line in fp.readlines() if len(line.strip()) !=0]
-    for line in tqdm(lines):
-        doc = tokenize(line)
-        #print(doc)
-        items.append(doc)
-    return items
+    labels = ["O"] * len(tokens)
+    for entity in entities:
+        ent_start = entity["start_char"]
+        ent_end = entity["end_char"]
+        etype = entity["entityType"]
+        mention = entity["mention"]
+
+        entity_start = -1
+        entity_end = -1
+        for i in range(len(tokens)):
+            if tokens[i][1] == ent_start:
+                entity_start = i
+                for j in range(i, len(tokens)):
+                    if tokens[j][1] + len(tokens[j][0]) == ent_end:
+                        entity_end = j
+
+        print(f"{entity_start}:{entity_end}:{etype}")
+        if entity_start != -1 and entity_end != -1:
+            labels[entity_start] = f"B-{etype}"
+            for i in range(entity_start+1, entity_end+1):
+                labels[i] = f"I-{etype}"
+
+
+    blabels = [1 if l != 'O' else 0 for l in labels]
+
+    print(tokens)
+    print(labels)
+    print(blabels)
+    return labels, blabels
+
     
-def load_dict(path):
-
-    items = []
-    with open(path) as fp:
-        entries = [line.strip().split('|') for line in fp.readlines() if len(line.strip()) !=0]
-
-    for entry in tqdm(entries):
-        atom_str = entry[0]
-        tokens_lc = tokenize(entry[1])
-        tokens_lc_head = tokenize(entry[2])
-        cui = entry[3]
-
-        # (atom string, list of atom tokens, list of head part of atom tokens, cui)
-        items.append((atom_str, tokens_lc, tokens_lc_head, cui))
-
-
-    return items
-    
-
-def match_entity(tokens, entity_dict, entity_type):
-    #print(tokens)
-    match_count = 0
-    n = len(tokens)
-    bio = ['O'] * n
-    for i in range(len(tokens)):
-        for entity in entity_dict[entity_type]:
-            if i+len(entity) > n-1:
-                continue
-            tgt = tokens[i:i+len(entity)]
-            if tuple(tgt) == tuple(entity):
-                #print('match at {}: {}'.format(i, '_'.join(entity)))
-                match_count += 1
-
-                if len(entity) == 1:
-                    bio[i] = f'S_{entity_type}'
-                else: 
-                    for j in range(i, i+len(entity)):
-                        if j == i:
-                            bio[j] = f'B_{entity_type}'
-                        else:
-                            bio[j] = f'I_{entity_type}'
-            
-    return bio, match_count
-        
-
 def annotate(json_data, parameters):
 
     ent2int = parameters["entity2integer"]
@@ -119,79 +101,33 @@ def annotate(json_data, parameters):
 
     outdir = parameters["output_dir"]
 
-
-    if parameters["use_dictionary"]:
-        for dict_path in dict_paths:
-            if not os.path.exists(dict_path):
-                continue
-
-            path, fname = os.path.split(dict_path)
-            name, txt = os.path.splitext(fname)
-
-            print(f"loading dictionary {dict_path}")
-            entity_dict[name] += load_dict(dict_path)
-
-        # if parameters["task_name"] == "bc5cdr":
-        if parameters["task_name"] == "bc5cdr" or parameters["task_name"] == "ncbi":
-            dict_dirs = parameters["dict_dir"]
-            other_dict_files= parameters["other_dict_files"]
-            dict_paths = [os.path.join(dict_dir, file) for dict_dir in dict_dirs for file in other_dict_files ]
-            for dict_path in dict_paths:
-                if not os.path.exists(dict_path):
-                    continue
-
-                path, fname = os.path.split(dict_path)
-                name, txt = os.path.splitext(fname)
-                print(f"loading dictionary {dict_path}")
-                entity_dict[name] += load_dict(dict_path)
-
-    
-        
-    # convert dictionay item to list to tuple
-    for key, items in entity_dict.items():
-        l_token_seq = [tuple(item[1]) for item in items]
-        l_token_seq += [tuple(item[2]) for item in items]
-        
-        entity_dict[key] = list(set(l_token_seq))
-
-    # sort dictionary items in ascending order
-    for key in entity_dict.keys():
-        entity_dict[key] = sorted(entity_dict[key], key =lambda x: -len(x))
+    ids = json_data["pmid"].keys()
 
 
-    match_count = defaultdict(int)
-    for file in tqdm(files):
-        
-        path, fname = os.path.split(file)
-        with open(file) as fp:
-                text = fp.read().strip()
-        #print(text)
-        doc = sentence_split(text)
-        
+    for id_ in sorted(ids):
+        pmid = json_data["pmid"][id_]
+        text = json_data["text"][id_]
+        entities = json_data["entities"][id_]
+
+        fname = f"{pmid}.txt"
+        doc = sentence_split(text.strip(), offset=True)
+
+        print(fname)
         with open(os.path.join(outdir, fname), 'w') as fp:
-            for k, sent in enumerate(doc):
-                tokens = tokenize(sent)
-                tokens_low = [token.lower() for token in tokens]
-                
+            for k, (sent, sent_offset) in enumerate(doc):
 
-                bio_tag = {}
-                for entity_type in entity_dict.keys():
-                    bio_tag[entity_type], cnt = match_entity(tokens_low, entity_dict, entity_type)
-                    match_count[entity_type] += cnt
-                    
-                #print(bio_tag)
-                labels = [tags for tags in zip(*bio_tag.values())]
-                    
-                for token, bio_label in zip(tokens, labels):
-                    buf = "{}\t{}".format(token, token.lower())
-                    
-                    for tag in bio_label:
-                        buf +=  "\t{}".format(tag)
-                    #print(buf)
-                    fp.write("{}\n".format(buf))    
-                fp.write('\n')
-                
-    return match_count
+                print("sent:", sent, len(sent))
+                tokens = tokenize(sent, offset=True)
+                tokens = [(token, sent_offset + offset_) for token, offset_ in tokens]
+    
+                labels, blabels = annotate_helper_(tokens, entities)
+
+                for token_, label_, blabel_ in zip(tokens, labels, blabels):
+                    fp.write(f"{token_[0]}\t{label_}\t{blabel_}\n")
+                fp.write("\n")
+
+
+    return 
 
     
 def main():
