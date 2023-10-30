7d4a3564b175737487d4589866f6557200f54c23
aae14935wb <aae14935wb@es1.abci.local>
2023-10-30 18:07:25 +0900

    commit

--- modules/preprocess/annotation/ds_annotation_spark2.py
+++ modules/preprocess/annotation/ds_annotation_spark2.py
@@ -12,8 +12,6 @@ import spacy
 
 
 from pyspark import SparkContext
-from pyspark.sql import SparkSession
-from pyspark.sql.types import StructType, StructField, StringType, IntegerType
 
 
 from sklearn.model_selection import train_test_split
@@ -27,11 +25,6 @@ from scispacy.abbreviation import AbbreviationDetector
 import scispacy
 from spacy.lang.en import English
 
-import nltk
-# nltk.download("wordnet", quiet=True)
-from nltk import word_tokenize
-# nltk.download('punkt')
-from nltk.corpus import wordnet as wn
 from nltk.stem import PorterStemmer, WordNetLemmatizer, LancasterStemmer
 
 
@@ -42,6 +35,8 @@ from snorkel.analysis import get_label_buckets
 from snorkel.labeling.model import LabelModel
 from snorkel.labeling.model import MajorityLabelVoter
 from snorkel.labeling.model import LabelModel
+from snorkel.labeling import LabelingFunction
+
 
 from snorkel.labeling.apply.spark import SparkLFApplier
 
@@ -54,56 +49,15 @@ from entity_extract2 import ExtractEntityCandidate
 
 import itertools
 
-
 import pdb
 
 
 ABSTAIN = -1
 
-# cancer immunology
-CYTOKINE = 0
-TRANSCRIPTION_FACTOR = 1
-T_LYMPHOCYTE = 2
-
-# jnlpba
-PROTEIN = 0
-CELL_LINE = 1
-CELL = 2
-DNA = 3
-RNA = 4
-
-# BC5CDR
-CHEMICAL = 0
-DISEASE = 1
-
-
-# threshold for edit distance
-max_dist = 0
-# max_dist=1
-
 # nlp = spacy.load("en_core_sci_lg")
 nlp = spacy.load("en_core_sci_sm")
-
 # nlp.add_pipe("abbreviation_detector")
 nlp.add_pipe("sentencizer")
-
-lemmatizer = WordNetLemmatizer()
-
-
-lemmatizer = WordNetLemmatizer()
-
-
-def is_substr(ref, src):
-
-    ref = re.sub(r'\W', '', ref)
-    src = re.sub(r'\W', '', src)
-
-    if src in ref:
-        return True
-    else:
-        return False
-
-
 
 def min_edit_distance(ref, src):
     # ref : Sting dictionary entry
@@ -112,6 +66,24 @@ def min_edit_distance(ref, src):
     min_l = ed.eval(ref, src)
     return min_l
 
+
+def keyword_lookup(x, domain_dict, max_dist, label):
+    # Returns a label of rating if pattern of digit star's found in the phrase
+
+    ent = x.lower()
+    for phrase in domain_dict:
+        if min_edit_distance(phrase, ent) <= max_dist:
+            return label
+    return ABSTAIN
+
+
+def make_keyword_lf(domain_dict, max_dist, label):
+
+    return LabelingFunction(
+        name = f"keyword_lf_{label}",
+        f=keyword_lookup,
+        resources=dict(domain_dict=domain_dict, max_dist=max_dist, label=label)
+    )
 
 
 def entity_extract(entityExtraction, sent, pmid, k):
@@ -144,185 +116,9 @@ def entity_extract(entityExtraction, sent, pmid, k):
     return df
 
 
-# snorkel Labeling functions
-@labeling_function()
-def lf_cytokine_distsv(x):
-    # Returns a label of rating if pattern of digit star's found in the phrase
-    ent = x.lower()
-    for phrase in dist_dict['cytokine.dict']:
-        # if ed.eval(ent,phrase.lower()) <= max_dist:
-        if min_edit_distance(phrase, ent) <= max_dist:
-            return CYTOKINE
-    return ABSTAIN
-
-
-@labeling_function()
-def lf_cytokine_substr(x):
-    # Returns a label of rating if pattern of digit star's found in the phrase
-    ent = x.lower()
-    for phrase in dist_dict['cytokine.dict']:
-        if is_substr(phrase, ent):
-            return CYTOKINE
-    return ABSTAIN
-
-
-@labeling_function()
-def lf_transcription_factor_distsv(x):
-    # Returns a label of rating if pattern of digit star's found in the phrase
-    ent = x.lower()
-    for phrase in dist_dict['transcription_factor.dict']:
-        # if ed.eval(ent,phrase.lower()) <= max_dist:
-        if min_edit_distance(phrase, ent) <= max_dist:
-            return TRANSCRIPTION_FACTOR
-    return ABSTAIN
-
-
-@labeling_function()
-def lf_transcription_factor_substr(x):
-    # Returns a label of rating if pattern of digit star's found in the phrase
-    ent = x.lower()
-    for phrase in dist_dict['transcription_factor.dict']:
-        if is_substr(phrase, ent):
-            return TRANSCRIPTION_FACTOR
-    return ABSTAIN
-
-
-@labeling_function()
-def lf_t_lymphocyte_distsv(x):
-    # Returns a label of rating if pattern of digit star's found in the phrase
-    ent = x.lower()
-    for phrase in dist_dict['t_lymphocyte.dict']:
-        # if ed.eval(ent,phrase.lower()) <= max_dist:
-        if min_edit_distance(phrase, ent) <= max_dist:
-            return T_LYMPHOCYTE
-    return ABSTAIN
-
-
-@labeling_function()
-def lf_t_lymphocyte_substr(x):
-    # Returns a label of rating if pattern of digit star's found in the phrase
-    ent = x.lower()
-    for phrase in dist_dict['t_lymphocyte.dict']:
-        if is_substr(phrase, ent):
-            return T_LYMPHOCYTE
-    return ABSTAIN
-
-
-@labeling_function()
-def lf_protein_distsv(x):
-    # Returns a label of rating if pattern of digit star's found in the phrase
-    ent = x.lower()
-    for phrase in dist_dict['protein.dict']:
-        # if ed.eval(ent,phrase.lower()) <= max_dist:
-        if min_edit_distance(phrase, ent) <= max_dist:
-            return PROTEIN
-    return ABSTAIN
-
-
-@labeling_function()
-def lf_cell_line_distsv(x):
-    # Returns a label of rating if pattern of digit star's found in the phrase
-    ent = x.lower()
-    for phrase in dist_dict['cell_line.dict']:
-        # if ed.eval(ent,phrase.lower()) <= max_dist:
-        if min_edit_distance(phrase, ent) <= max_dist:
-            return CELL_LINE
-    return ABSTAIN
-
-
-@labeling_function()
-def lf_cell_distsv(x):
-    # Returns a label of rating if pattern of digit star's found in the phrase
-    ent = x.lower()
-    for phrase in dist_dict['cell.dict']:
-        # if ed.eval(ent,phrase.lower()) <= max_dist:
-        if min_edit_distance(phrase, ent) <= max_dist:
-            return CELL
-    return ABSTAIN
-
-
-@labeling_function()
-def lf_dna_distsv(x):
-    # Returns a label of rating if pattern of digit star's found in the phrase
-    ent = x.lower()
-    for phrase in dist_dict['dna.dict']:
-        # if ed.eval(ent,phrase.lower()) <= max_dist:
-        if min_edit_distance(phrase, ent) <= max_dist:
-            return DNA
-    return ABSTAIN
-
-
-@labeling_function()
-def lf_rna_distsv(x):
-    # Returns a label of rating if pattern of digit star's found in the phrase
-    ent = x.lower()
-    for phrase in dist_dict['rna.dict']:
-        # if ed.eval(ent,phrase.lower()) <= max_dist:
-        if min_edit_distance(phrase, ent) <= max_dist:
-            return RNA
-    return ABSTAIN
-
-
-@labeling_function()
-def lf_debug(x):
-    ent = x.lower()
-    for phrase in dist_dict['rna.dict']:
-        # if ed.eval(ent,phrase.lower()) <= max_dist:
-        if min_edit_distance(phrase, ent) <= max_dist:
-            # l = ed.eval(ent, phrase)
-            # if l == 0:
-            return RNA
-    return ABSTAIN
-
-
-@labeling_function()
-def lf_chemicals_distsv(x):
-    # Returns a label of rating if pattern of digit star's found in the phrase
-    ent = x.lower()
-    for phrase in dist_dict['chemicals.dict']:
-        # if ed.eval(ent,phrase.lower()) <= max_dist:
-        if min_edit_distance(phrase, ent) <= max_dist:
-            return CHEMICAL
-    return ABSTAIN
-
-
-@labeling_function()
-def lf_chemicals_substr(x):
-    # Returns a label of rating if pattern of digit star's found in the phrase
-    ent = x.lower()
-    for phrase in dist_dict['chemicals.dict']:
-        if is_substr(phrase, ent):
-            return CHEMICAL
-    return ABSTAIN
-
-
-@labeling_function()
-def lf_disease_distsv(x):
-    # Returns a label of rating if pattern of digit star's found in the phrase
-    ent = x.lower()
-    for phrase in dist_dict['disease.dict']:
-        # if ed.eval(ent,phrase.lower()) <= max_dist:
-        if min_edit_distance(phrase, ent) <= max_dist:
-            return DISEASE
-    return ABSTAIN
-
-
-@labeling_function()
-def lf_disease_substr(x):
-    # Returns a label of rating if pattern of digit star's found in the phrase
-    ent = x.lower()
-    for phrase in dist_dict['disease.dict']:
-        if is_substr(phrase, ent):
-            return DISEASE
-    return ABSTAIN
-
-
-global dist_dict
 
 
 def snorkel(parameters, df_train, lfs):
-
-    global dist_dict
 
     lf_applier = PandasLFApplier(lfs=lfs)
     L_train = lf_applier.apply(df=df_train)
@@ -332,7 +128,6 @@ def snorkel(parameters, df_train, lfs):
 
 def snorkel_spark(parameters, df_train, lfs):
 
-    global dist_dict
 
     sc = SparkContext()
     rdd = sc.parallelize(df_train)
@@ -344,7 +139,6 @@ def snorkel_spark(parameters, df_train, lfs):
 
 def main():
 
-    global dist_dict
 
     # check running time
     t_start = time.time()
@@ -378,7 +172,7 @@ def main():
                     dfs.append(df)
                 except:
                     print("Exception, entity_extract")
-                    
+
 
     df_train = pd.concat(dfs, ignore_index=True)
 
@@ -388,7 +182,8 @@ def main():
     dict_dirs = parameters["processed_dict_dirs"]
     dictionary_files = parameters["dict_files"]
 
-    dist_dict = defaultdict(list)
+    domain_dict = defaultdict(list)
+
     for fname in dictionary_files:
 
         terms = []
@@ -399,17 +194,22 @@ def main():
             entries = sum([line.split('|')[1:3] for line in lines], [])
             terms += entries
 
-        dist_dict[fname] = sorted(list(set(terms)))
+        entity_type = fname.replace('.dict','').upper()
+        domain_dict[entity_type] = sorted(list(set(terms)))
+
 
     # snorkel labeling functions
     dictionary_files = parameters["dict_files"]
     lfs = []
-    for fname in dictionary_files:
+    for label, fname in enumerate(dictionary_files):
         basename, _ = os.path.splitext(fname)
-        lf_func = f"lf_{basename}_distsv"
-        lfs.append(eval(lf_func))
-        # lf_func = f"lf_{basename}_substr"
-        # lfs.append(eval(lf_func))
+
+        entity_type = basename.replace('.dict','').upper()
+        min_dist = 0
+
+        keyword_f = make_keyword_lf(domain_dict[entity_type], min_dist, label)
+
+        lfs.append(keyword_f)
 
     if parameters["spark"]:
         L_train = snorkel_spark(parameters, df_train["entities"], lfs)
