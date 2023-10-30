1b3e84d841fa30672b4fef6204ae92ac5bb6defc
aae14935wb <aae14935wb@es2.abci.local>
2023-10-30 17:01:56 +0900

    commit

---
+++ scripts/immunology_preprocess_ds_annotation2a.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+set -e
+set -u
+
+HOME=$PWD
+export PYTHONPATH="$HOME:$HOME/modules"
+
+TASK="immunology"
+SUBTASK="preprocess"
+MODULE="ds_annotation2"
+
+CONFIG_DIR="configs"
+YAML_FILE="${TASK}_${SUBTASK}_${MODULE}.yaml"
+YAML_PATH="${CONFIG_DIR}/${YAML_FILE}"
+LOG_DIR="experiments/$TASK/logs"
+LOG_PATH="${LOG_DIR}/${TASK}_${SUBTASK}_${MODULE}.log"
+
+if [ ! -d $LOG_DIR ]; then
+    mkdir -p $LOG_DIR
+fi
+
+spark-submit modules/preprocess/annotation/ds_annotation_spark2a.py --yaml $YAML_PATH 
+
+#sleep 5
+#tail -f $LOG_PATH
+
