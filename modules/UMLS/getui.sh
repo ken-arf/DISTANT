3aa2a4f6cf4797e4158512a3565f8584e0e2112e
aae14935wb <aae14935wb@es2.abci.local>
2024-01-25 22:51:16 +0900

    commit

--- modules/UMLS/getui.sh
+++ modules/UMLS/getui.sh
@@ -3,5 +3,5 @@
 #python search-terms.py -k 3ebafc45-4529-4619-92a3-e3c9432b9e03 -s disease > disease.result
 #python search-terms.py -k 3ebafc45-4529-4619-92a3-e3c9432b9e03 -s chemicals > chemicals.result
 
-cat chemicals.result| grep -e "MSH" -e "SNOMED" -e "RXNORM" -B 3 | grep ^UI | tr -d "UI: " > chemical.ui
-cat disease.result| grep -e "MSH" -e "SNOMED" -e "RXNORM" -B 3 | grep ^UI  | tr -d "UI:" > disease.ui
+cat chemicals.result| grep -e "MTH" -e "MSH" -e "SNOMED" -e "RXNORM" -e "ICD10" -B 3 | grep ^UI | tr -d "UI: " > chemical.ui
+cat disease.result| grep -e "MTH" -e "MSH" -e "SNOMED" -e "RXNORM" -e "ICD10" -B 3 | grep ^UI  | tr -d "UI:" > disease.ui
