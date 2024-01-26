428665d82e5e33b7fddbce686c0b2e4cba0fdc40
aae14935wb <aae14935wb@es1.abci.local>
2024-01-26 10:42:20 +0900

    commit

--- modules/UMLS/getui.sh
+++ modules/UMLS/getui.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+#python search-terms.py -k 3ebafc45-4529-4619-92a3-e3c9432b9e03 -s "T tcell" > tcell.result
+#python search-terms.py -k 3ebafc45-4529-4619-92a3-e3c9432b9e03 -s "cytokine" > cytokine.result
+#python search-terms.py -k 3ebafc45-4529-4619-92a3-e3c9432b9e03 -s "transcription factor" > transcription_factor.result
+
 #python search-terms.py -k 3ebafc45-4529-4619-92a3-e3c9432b9e03 -s disease > disease.result
 #python search-terms.py -k 3ebafc45-4529-4619-92a3-e3c9432b9e03 -s chemicals > chemicals.result
 
